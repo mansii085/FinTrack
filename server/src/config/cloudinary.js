54e1dddce1bafc4e4cee3ba46c669048feb13e2a
mansii085 <mansimurvariya012@gmail.com>
2026-07-16 06:50:42 +0530

    cloudinary file added

---
+++ server/src/config/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "fintrack/avatars",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    transformation: [{ width: 300, height: 300, crop: "fill", gravity: "face" }],
+  },
+});
+
+export { cloudinary, storage };
